9b18d518a5665b3ec849f9b95acf174e06c4a2a7
FlorGonz861 <gonzalez.florencia.861@hotmail.com>
2022-07-31 01:59:15 -0300

    arreglos

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -18,15 +18,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
-import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
-import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
-import { NewEducacionComponent } from './components/educacion/new-educacion.component';
-import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
-import { EditacercaComponent } from './components/acerca-de/editacerca.component';
-import { EditproyectoComponent } from './components/proyecto/editproyecto.component';
-import { NewProyectoComponent } from './components/proyecto/new-proyecto.component';
-import { NewhysComponent } from './components/hys/newhys.component';
-import { EdithysComponent } from './components/hys/edithys.component';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia/edit-experiencia.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion/new-educacion.component';
+import { EditEducacionComponent } from './components/educacion/edit-educacion/edit-educacion.component';
+import { EditacercaComponent } from './components/acerca-de/editacerca/editacerca.component';
+import { EditproyectoComponent } from './components/proyecto/editproyecto/editproyecto.component';
+import { NewProyectoComponent } from './components/proyecto/new-proyecto/new-proyecto.component';
+import { NewhysComponent } from './components/hys/newhys/newhys.component';
+import { EdithysComponent } from './components/hys/edithys/edithys.component';
 
 @NgModule({
   declarations: [
@@ -51,7 +51,7 @@ import { EdithysComponent } from './components/hys/edithys.component';
     NewProyectoComponent,
     NewhysComponent,
     EdithysComponent,
-         
+
   ],
   imports: [
     BrowserModule,
